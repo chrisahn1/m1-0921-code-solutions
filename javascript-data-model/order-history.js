bc6b43b392ab4a9c4bdeec5b42d37ee84030bc47
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2021-10-22 05:30:12 -0700

    Completed js data model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,60 @@
+var orderHistory = [
+  {
+    delivered_date: ['Aug', 8, 2020],
+    order_placed: ['Aug', 4, 2020],
+    order_number: '114-3941689-8772232',
+    items: [
+      {
+        title: 'Javascript for impatient programmers',
+        author: 'Dr. Axel Rauschmayer',
+        price: 31.55,
+        return_window: ['Sept', 7, 2020]
+      }
+    ]
+  },
+  {
+    delivered_date: ['Jul', 20, 2020],
+    order_placed: ['Jul', 19, 2020],
+    order_number: '113-9984268-1280257',
+    items: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Christopher Alexander',
+        price: 41.33,
+        return_window: ['Aug', 19, 2020]
+      }
+    ]
+  },
+  {
+    delivered_date: ['Jul', 7, 2020],
+    order_placed: ['Jul', 4, 2020],
+    order_number: '114-2875557-9059409',
+    items: [
+      {
+        title: 'Gamecube Controller Adapter...',
+        author: '',
+        price: 15.98,
+        return_window: ['Aug', 5, 2020]
+      }
+    ]
+  },
+  {
+    delivered_date: ['Jul', 5, 2020],
+    order_placed: ['Jul', 3, 2020],
+    order_number: '113-2883177-2648248',
+    items: [
+      {
+        title: 'Gamecube Controller...',
+        author: '',
+        price: 94.95,
+        return_window: ['Aug', 4, 2020]
+      },
+      {
+        title: 'The Art of Sql',
+        author: 'Stephane Faroult',
+        price: 33.99,
+        return_window: ['Aug', 4, 2020]
+      }
+    ]
+  }
+];
