02a152a0923a24fb26884e2b4a4fde681cbd04ca
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2021-11-04 21:23:15 -0700

    Completed dom events

---
+++ dom-events/buttons.js
@@ -0,0 +1,26 @@
+function handleClick(event) {
+  console.log('button Clicked');
+  console.log('event: ', event);
+  console.log('event.target: ', event.target);
+}
+
+var element1 = document.querySelector('.click-button');
+element1.addEventListener('click', handleClick);
+
+function handleMouseover(event) {
+  console.log('button hovered');
+  console.log('event: ', event);
+  console.log('event.target: ', event.target);
+}
+
+var element2 = document.querySelector('.hover-button');
+element2.addEventListener('mouseover', handleMouseover);
+
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log('event: ', event);
+  console.log('event.target: ', event.target);
+}
+
+var element3 = document.querySelector('.double-click-button');
+element3.addEventListener('dblclick', handleDoubleClick);
