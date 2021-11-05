9535e1fa3ce80b043b9767c6bc69a6d15123ac54
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2021-11-04 19:19:48 -0700

    Completed dom querying

---
+++ dom-querying/main.js
@@ -0,0 +1,20 @@
+console.log('hello, world');
+
+var element1 = document.querySelector('h1');
+
+console.log(element1);
+// console.dir(element1);
+
+var element2 = document.querySelector('#explanation');
+console.log(element2);
+// console.dir(element2);
+
+var element3 = document.querySelector('.hint');
+console.log(element3);
+// console.dir(element3);
+
+var element4 = document.querySelectorAll('p');
+console.log(element4);
+
+var element5 = document.querySelectorAll('.example-link');
+console.log(element5);
