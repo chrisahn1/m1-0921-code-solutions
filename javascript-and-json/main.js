712e563f723dd47f4fe5b4fecdd1e53fc8e26980
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2021-11-23 18:41:35 -0800

    Updated 2 js json

--- javascript-and-json/main.js
+++ javascript-and-json/main.js
@@ -24,15 +24,8 @@ for (let i = 0; i < arr.length; i++) {
   }
 }
 
-var arr1 = ['{"isbn": 111111, "title": "Harry Potter", "author": "J.K. Rowlings"}',
-  '{"isbn": 222222, "title": "Lord of the Rings", "author": "J.R.R. Tolkien"}',
-  '{"isbn": 333333, "title": "1984", "author": "George Orwell"}'
-];
-
-for (let i = 0; i < arr1.length; i++) {
-  console.log('typeof arr1: ', typeof arr1[i]);
-  console.log(arr1[i]);
-}
+var bookJSON = JSON.stringify(arr);
+console.log('typeof bookJSON: ', typeof bookJSON);
 
 var obj = '{"id": 1234, "name": "Chris Ahn"}';
 
